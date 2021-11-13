0e07d18f376bfe926a150bb94cf8969f0a83f5e5
DanStus <57114167+DanStus@users.noreply.github.com>
2021-11-13 16:22:57 -0500

    Added some simple chunks + basic level generator stuff

--- src/levelGenerators/LeandresStusalitusGenerator/LevelGenerator.java
+++ src/levelGenerators/LeandresStusalitusGenerator/LevelGenerator.java
@@ -4,12 +4,37 @@ import engine.core.MarioLevelGenerator;
 import engine.core.MarioLevelModel;
 import engine.core.MarioTimer;
 
+import java.util.Random;
+
 public class LevelGenerator implements MarioLevelGenerator {
+
+    Random rand;
 
     public LevelGenerator(){}
 
     @Override
     public String getGeneratedLevel(MarioLevelModel model, MarioTimer timer) {
+        this.rand = new Random();
+        model.clearMap();
+
+        char lastPiece = 'a';
+
+        while(lastPiece != 'd'){
+            System.out.print(lastPiece);
+            double d = rand.nextDouble();
+            switch(lastPiece){
+                case 'a':
+                    lastPiece = 'b';
+                    break;
+                case 'b':
+                    if(d >0.5)
+                        lastPiece = 'c';
+                    else
+                        lastPiece = 'b';
+                    break;
+            }
+        }
+
         return null;
     }
 
