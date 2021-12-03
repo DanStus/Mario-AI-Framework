7c14e25274bf5d319981852ae39f4a64f8ec95e1
DanStus <57114167+DanStus@users.noreply.github.com>
2021-12-03 12:38:06 -0500

    Added overrided methods

--- src/agents/LeandresStusalitus/Agent.java
+++ src/agents/LeandresStusalitus/Agent.java
@@ -1,4 +1,26 @@
 package agents.LeandresStusalitus;
 
-public class Agent {
+import engine.core.MarioAgent;
+import engine.core.MarioForwardModel;
+import engine.core.MarioTimer;
+import engine.helper.MarioActions;
+
+public class Agent implements MarioAgent {
+
+    private boolean[] action;
+
+    @Override
+    public void initialize(MarioForwardModel model, MarioTimer timer) {
+        this.action = new boolean[MarioActions.numberOfActions()];
+    }
+
+    @Override
+    public boolean[] getActions(MarioForwardModel model, MarioTimer timer) {
+        return new boolean[0];
+    }
+
+    @Override
+    public String getAgentName() {
+        return "LeandresStusalitus";
+    }
 }
