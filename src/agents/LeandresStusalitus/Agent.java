aa46b7da9e92f75fd2bc1d33e6064403e24404d2
DanStus <57114167+DanStus@users.noreply.github.com>
2021-12-06 18:01:16 -0500

    Mario can now jump gaps

--- src/agents/LeandresStusalitus/Agent.java
+++ src/agents/LeandresStusalitus/Agent.java
@@ -11,6 +11,25 @@ public class Agent implements MarioAgent {
 
     private boolean[] action;
 
+    private boolean jumpGap(int[] marioPos, int[][] level){
+        // Check for gaps, starting at Mario's x position and looking ahead 2
+        // Check from Mario's y pos down to the bottom of the level
+        // If no blocks are found there must be a gap
+        for (int x = marioPos[0]+1; x <= marioPos[0] + 2; x++){
+            for(int y = marioPos[1]; y <= 15; y++){
+                // If we find a block, then there is no gap
+                // Skip to the next X coord
+                if(level[x][y] != 0){
+                    break;
+                }
+                else if (y == 15){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     @Override
     public void initialize(MarioForwardModel model, MarioTimer timer) {
         this.action = new boolean[MarioActions.numberOfActions()];
@@ -31,6 +50,10 @@ public class Agent implements MarioAgent {
         int[][] level = model.getScreenSceneObservation(2);
         if(level[pos[0]+1][pos[1]] != 0 || level[pos[0]+2][pos[1]] != 0)
             return new boolean[]{false, true, false, true, true};
+
+        if(jumpGap(pos, level)){
+            return new boolean[]{false, true, false, true, true};
+        }
 
         return new boolean[]{false, true, false, true, false};
     }
